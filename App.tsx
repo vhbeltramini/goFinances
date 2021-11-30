8b957b85fb45dee99302094f41d73a9ef6908e3c
Victor Hugo Grabowski Beltramini <vhbeltramini@gmail.com>
2021-11-30 18:52:05 -0300

    Sumary cards Update

--- App.tsx
+++ App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme';
 import { Welcome } from './src/components/Welcome';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return <Dashboard />
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,31 +1,64 @@
 import React from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
-} from 'react-native';
+import { HighlightCard } from "../../components/HighlightCard";
 
 import { 
-    Container, 
-    Title 
+    Container,
+    Header,
+    UserContainer,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    IconSignOff,
+    HighlightCards,
+    Transactions,
+    Title
 } from './styles';
 
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserContainer>
+                    <UserInfo>
+                        <Photo source={{uri: 'https://avatars.githubusercontent.com/u/55711918?v=4'}} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Victor</UserName>
+                        </User>
+                    </UserInfo>
+                    <IconSignOff name="power" />
+                </UserContainer>
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard 
+                    title="Entradas" 
+                    amount="R$ 17.400,00" 
+                    lastTransaction="Última entrada dia 11 de outubro" 
+                    type="up"
+                />
+                <HighlightCard 
+                    title="Saídas" 
+                    amount="R$ 1.259,00" 
+                    lastTransaction="Última saida dia 03 de outubro" 
+                    type="down"
+                />
+                <HighlightCard 
+                    title="Total" 
+                    amount="R$ 16.141,00" 
+                    lastTransaction="01 a 16 de abril" 
+                    type="total"
+                />
+            </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+            </Transactions>
+
         </Container>
     );
 
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
